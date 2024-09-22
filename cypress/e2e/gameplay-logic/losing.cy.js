60356ecf92efda65e76eb65a01e15e3fc9145946
cheuni02 <inhcheung@gmail.com>
2024-09-22 14:59:27 +0100

    feat(e2e): loses test

--- cypress/e2e/gameplay-logic/losing.cy.js
+++ cypress/e2e/gameplay-logic/losing.cy.js
@@ -1,1 +1,20 @@
-describe("what should happen when a player loses", () => {});
+describe("what should happen when a player loses", () => {
+  beforeEach(() => {
+    cy.visit("/");
+    cy.get(".game-board").should("be.visible");
+  });
+
+  it("when O loses O cannot continue to play the game", () => {
+    cy.contains("Next player")
+      .then(() => {
+        cy.selectSquare(0, 0); // X
+        cy.selectSquare(1, 0); // O
+        cy.selectSquare(0, 1); // X
+        cy.selectSquare(1, 1); // O
+        cy.selectSquare(0, 2); // X
+      })
+      .selectSquare(1, 2) // O attempts to play on this square
+      .getSquareText(1, 2)
+      .then((text) => expect(text).to.eq(""));  
+  });
+});
